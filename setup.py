9c2e3ae9d188bcb934521a615e567b1cab44466b
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-05-16 15:34:27 +0200

    REL: setup.py added

---
+++ setup.py
@@ -0,0 +1,92 @@
+# Copyright (C) 2017  Joscha Reimer jor@informatik.uni-kiel.de
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""A setuptools based setup module.
+https://packaging.python.org/en/latest/distributing.html
+"""
+
+import setuptools
+import os.path
+
+# Get the long description from the README file
+readme_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+with open(readme_file, mode='r', encoding='utf-8') as f:
+    long_description = f.read()
+
+# Version string
+def version():
+    import setuptools_scm
+    def empty_local_scheme(version):
+        return ""
+    return {'local_scheme': empty_local_scheme}
+
+
+setuptools.setup(
+    # Name
+    name = 'matrix-decomposition',
+    
+    # Desctiption
+    description = 'allows to decompose (factorize) dense and sparse matrices',
+    long_description = long_description,
+
+    # Keywords
+    keywords = 'decomposition factorization decompose factorize dense sparse matrix matrices',
+
+    # Homepage
+    url = 'https://github.com/jor-/matrix_decomposition',
+
+    # Author
+    author = 'Joscha Reimer',
+    author_email = 'jor@informatik.uni-kiel.de',
+
+    # Version
+    use_scm_version = version,
+
+    # License
+    license = 'AGPLv3+',
+
+    # Classifiers
+    classifiers = [
+        # Development Status
+        'Development Status :: 3 - Alpha',
+
+        # Intended Audience, Topic
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+
+        # Licence (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)'
+
+        # Supported Python versions
+        'Programming Language :: Python',
+    ],
+
+    # Packages to install
+    packages = setuptools.find_packages(),
+
+    # Dependencies
+    setup_requires = [
+        'setuptools>=0.8',
+        'pip>=1.4',
+        'setuptools_scm',
+    ],
+    install_requires = [
+        'numpy',
+        'scipy>=0.19',
+    ],
+)
