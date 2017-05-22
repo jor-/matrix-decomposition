e995fd601d676d7e0d06ac7506f3e4e562bc6c9a
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-05-22 14:29:38 +0200

    TEST: tests for approximation function added

--- matrix/tests/test_all.py
+++ matrix/tests/test_all.py
@@ -22,13 +22,16 @@ def random_matrix(n, dense=True):
     return A
 
 
-def random_square_matrix(n, dense=True, positive_semi_definite=False, positive_definite=False):
+def random_square_matrix(n, dense=True, positive_semi_definite=False, positive_definite=False, min_diag_value=None):
     A = random_matrix(n, dense=dense)
     A = A + A.H
     if positive_semi_definite or positive_definite:
         A = A @ A
-    if positive_definite:
-        min_diag_value = 1
+    if min_diag_value is not None or positive_definite:
+        if min_diag_value is None:
+            min_diag_value = 0
+        if positive_definite:
+            min_diag_value = max(min_diag_value, 1)
         for i in range(n):
             A[i, i] += min_diag_value
     return A
@@ -183,3 +186,50 @@ def test_positive_definite(n, dense):
     assert not matrix.is_positive_semi_definite(-A)
     assert matrix.is_positive_definite(A)
     assert not matrix.is_positive_definite(-A)
+
+
+# *** approximate *** #
+
+test_approximate_setups = [
+    (n, dense, permutation_method, check_finite, return_type, t, min_diag_value, max_diag_value_shift, min_abs_value)
+    for n in (10,)
+    for dense in (True, False)
+    for permutation_method in supported_permutation_methods(dense)
+    for check_finite in (True, False)
+    for return_type in matrix.DECOMPOSITION_TYPES
+    for min_abs_value in (None, 10**-8)
+    for min_diag_value in (None, 10**-4)
+    for max_diag_value_shift in (None, 1 + np.random.rand(1) * 10)
+    for t in (None, np.random.rand(n) + 10**-4)
+]
+
+
+@pytest.mark.parametrize('n, dense, permutation_method, check_finite, return_type, t, min_diag_value, max_diag_value_shift, min_abs_value', test_approximate_setups)
+def test_approximate(n, dense, permutation_method, check_finite, return_type, t, min_diag_value, max_diag_value_shift, min_abs_value):
+    if t is None:
+        if min_diag_value is None:
+            A_min_diag_value = 10**-6
+        else:
+            A_min_diag_value = min_diag_value
+    else:
+        A_min_diag_value = None
+        assert min_diag_value is None or min_diag_value <= t.min()
+
+    A = random_square_matrix(n, dense=dense, min_diag_value=A_min_diag_value)
+
+    if max_diag_value_shift is not None:
+        if t is None:
+            max_diag_value = A.diagonal().max()
+        else:
+            max_diag_value = t.max()
+        max_diag_value = max_diag_value + max_diag_value_shift
+    else:
+        max_diag_value = None
+
+    decomposition = matrix.approximate(A, t=t, min_diag_value=min_diag_value, max_diag_value=max_diag_value, min_abs_value=min_abs_value, permutation_method=permutation_method, check_finite=check_finite, return_type=return_type)
+    if min_diag_value is not None or max_diag_value is not None:
+        decomposition = decomposition.to(matrix.LDL_DECOMPOSITION_TYPE)
+        if min_diag_value is not None:
+            assert decomposition.d.min() >= min_diag_value
+        if max_diag_value is not None:
+            assert decomposition.d.max() <= max_diag_value
