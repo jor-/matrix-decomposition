375c34a30a25311bb0ec1cb1dcbe897123e0b6a3
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-05-22 14:28:56 +0200

    ENH: approximation function added

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -1,3 +1,3 @@
 from matrix.constants import *
 from matrix.sparse.constants import SPARSE_PERMUTATION_METHODS
-from matrix.calculate import decompose, is_positive_semi_definite, is_positive_definite
+from matrix.calculate import decompose, is_positive_semi_definite, is_positive_definite, approximate
