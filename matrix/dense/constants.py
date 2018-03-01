080daed1164252df72bf57c9eaa3b4c5c317c8b0
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-03-01 15:40:41 +0100

    DOC: matrix.*.constants: doc string for 'PERMUTATION_METHODS' added

--- matrix/dense/constants.py
+++ matrix/dense/constants.py
@@ -1,2 +1,3 @@
 from matrix.constants import DECOMPOSITION_TYPES
 from matrix.constants import UNIVERSAL_PERMUTATION_METHODS as PERMUTATION_METHODS
+""" Supported permutation methods for decompose dense matrices. """
