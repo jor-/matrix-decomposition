e12455ee43edb9cc2d1c2ba3422e63bb45a87085
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-07-20 13:51:34 +0200

    DOC: setup: copyright year updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017  Joscha Reimer jor@informatik.uni-kiel.de
+# Copyright (C) 2017-2018 Joscha Reimer jor@informatik.uni-kiel.de
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
