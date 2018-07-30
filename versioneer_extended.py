42a7929eeebb298b2d88310843533df3df8d101d
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-07-30 14:29:54 +0200

    BLD: versioneer: version is now Setuptools compliant

--- versioneer_extended.py
+++ versioneer_extended.py
@@ -3,7 +3,7 @@ import versioneer
 
 def get_version():
     """Get the short version string for this project."""
-    return versioneer.get_version().replace('.dev0+', '+dirty.').replace('.post', '.post.dev')
+    return versioneer.get_version().replace('.dev0+', '+dirty.').replace('.post', '.post0.dev')
 
 
 def get_cmdclass():
