389029e168b09c3c1f0641900658633e2c218e2e
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-06-12 19:51:28 +0200

    MAINT: matrix: __init__ rewritten

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -1,29 +1,31 @@
 # *** submodules *** #
 
-import matrix.constants
-import matrix.decompositions
-import matrix.errors
-
+from matrix import constants, decompositions, errors
 
 # *** functions *** #
 
-from matrix.calculate import decompose, is_positive_semidefinite, is_positive_definite, is_invertible, solve
+from matrix.calculate import (is_positive_semidefinite, is_positive_definite, is_invertible,
+                              decompose, solve)
 from matrix.approximate import decomposition, positive_definite_matrix
-
 
 # *** constants *** #
 
-from matrix.constants import LDL_DECOMPOSITION_TYPE, LDL_DECOMPOSITION_COMPRESSED_TYPE, LL_DECOMPOSITION_TYPE
+from matrix.constants import (
+    DECOMPOSITION_TYPES,
+    LDL_DECOMPOSITION_TYPE, LDL_DECOMPOSITION_COMPRESSED_TYPE, LL_DECOMPOSITION_TYPE,
+    UNIVERSAL_PERMUTATION_METHODS, SPARSE_ONLY_PERMUTATION_METHODS, APPROXIMATION_ONLY_PERMUTATION_METHODS,
+    NO_PERMUTATION_METHOD,
+    DECREASING_DIAGONAL_VALUES_PERMUTATION_METHOD, INCREASING_DIAGONAL_VALUES_PERMUTATION_METHOD,
+    DECREASING_ABSOLUTE_DIAGONAL_VALUES_PERMUTATION_METHOD,
+    INCREASING_ABSOLUTE_DIAGONAL_VALUES_PERMUTATION_METHOD)
 
-DECOMPOSITION_TYPES = matrix.constants.DECOMPOSITION_TYPES
+DECOMPOSITION_TYPES = DECOMPOSITION_TYPES
 """ Supported types of decompositions. """
-
-from matrix.constants import NO_PERMUTATION_METHOD, DECREASING_DIAGONAL_VALUES_PERMUTATION_METHOD, INCREASING_DIAGONAL_VALUES_PERMUTATION_METHOD, DECREASING_ABSOLUTE_DIAGONAL_VALUES_PERMUTATION_METHOD, INCREASING_ABSOLUTE_DIAGONAL_VALUES_PERMUTATION_METHOD
-UNIVERSAL_PERMUTATION_METHODS = matrix.constants.UNIVERSAL_PERMUTATION_METHODS
+UNIVERSAL_PERMUTATION_METHODS = UNIVERSAL_PERMUTATION_METHODS
 """ Supported permutation methods for decompose dense and sparse matrices. """
-SPARSE_ONLY_PERMUTATION_METHODS = matrix.constants.SPARSE_ONLY_PERMUTATION_METHODS
+SPARSE_ONLY_PERMUTATION_METHODS = SPARSE_ONLY_PERMUTATION_METHODS
 """ Supported permutation methods only for sparse matrices. """
-APPROXIMATION_ONLY_PERMUTATION_METHODS = matrix.constants.APPROXIMATION_ONLY_PERMUTATION_METHODS
+APPROXIMATION_ONLY_PERMUTATION_METHODS = APPROXIMATION_ONLY_PERMUTATION_METHODS
 """ Supported permutation methods only for approximate dense and sparse matrices. """
 
 
@@ -37,5 +39,5 @@ del get_versions
 # *** logging *** #
 
 import logging
-
 logger = logging.getLogger(__name__)
+del logging
